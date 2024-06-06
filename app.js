7e8f2746f5d6cbd20d6b1d8aa595c2cae45386b1
Srirag Hariharan <sriraghariharan108@gmail.com>
2024-06-06 15:26:41 +0530

    Commit number: 04

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 
 app.get('/test', (_req, res) =>{
-    res.status(200).send("Hello Srirag CI/CD version 01")
+    res.status(200).send("Hello Srirag CI/CD version 04")
 });
 
 module.exports = app;
